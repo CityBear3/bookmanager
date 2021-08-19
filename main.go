b23b5855b78f90a3f25ecfe1a51db9ee57a0b4ab
CityBear3 <68993998+CityBear3@users.noreply.github.com>
2021-08-19 14:25:41 +0900

    [add]entry point

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"app/controller"
+	"app/middleware"
+	"net/http"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		names := []string{"John", "Jane"}
+		c.SecureJSON(http.StatusOK, names)
+	})
+
+	store := cookie.NewStore([]byte("secret"))
+	router.Use(sessions.Sessions("mysession", store))
+
+	router.POST("/login", controller.Login)
+
+	authGroup := router.Group("/auth")
+	authGroup.Use(middleware.LoginCheck())
+	{
+		authGroup.GET("/hello", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"message": "Hello, World!!",
+			})
+		})
+	}
+
+	router.Run("127.0.0.1:8080")
+}
