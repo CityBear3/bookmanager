a38e823d75b265ec1e78ee889c6ea15a9d99d03c
CityBear3 <68993998+CityBear3@users.noreply.github.com>
2021-08-23 17:03:49 +0900

    [feat]dbからユーザー情報を取得

--- handler/login.go
+++ handler/login.go
@@ -1,7 +1,9 @@
 package handler
 
 import (
+	"app/aop"
 	"app/model"
+	"log"
 	"net/http"
 
 	"github.com/gin-contrib/sessions"
@@ -14,19 +16,28 @@ func Login(c *gin.Context) {
 		uid: string ユーザーID Repository実装までは使用しない
 		password: string パスワード
 	*/
-	var _ = c.PostForm("uid")
+	var uid = c.PostForm("uid")
 	var password = c.PostForm("password")
 
-	//仮のRepository
-	user := model.User{Uid: "user1", Password: "0000"}
-	hashedPass, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
+	db, err := aop.Connect()
+	if err != nil {
+		c.Status(http.StatusUnauthorized)
+	}
+
+	dbCloser, err := db.DB()
 	if err != nil {
 		c.Status(http.StatusInternalServerError)
 	}
+	defer dbCloser.Close()
+
+	user := model.User{Uid: uid}
+	db.Find(&user)
+	db.Logger.LogMode(4)
 
 	//パスワードを比較
-	err = bcrypt.CompareHashAndPassword(hashedPass, []byte(password))
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
 	if err != nil {
+		log.Fatalln(err)
 		c.Status(http.StatusForbidden)
 	} else {
 		//sessionを取得
