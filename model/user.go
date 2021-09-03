c19b3f43dd6aa794b74889778b511497db752d5c
CityBear3 <68993998+CityBear3@users.noreply.github.com>
2021-09-04 03:06:13 +0900

    [feat]ユーザー取得関数を追加

--- model/user.go
+++ model/user.go
@@ -1,7 +1,29 @@
 package model
 
+import (
+	"app/aop"
+)
+
 type User struct {
 	Id       uint   `gorm:"primary_key"`
 	Uid      string `gorm:"column:uid"`
 	Password string `gorm:"column:password"`
+}
+
+func (u *User) GetUserById() error {
+	db, err := aop.Connect()
+	if err != nil {
+		return err
+	}
+
+	dbCloser, err := db.DB()
+	if err != nil {
+		return err
+	}
+	defer dbCloser.Close()
+
+	db.Find(u)
+	db.Logger.LogMode(4)
+
+	return nil
 }
